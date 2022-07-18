c1473199e67c65925393de084e713679571dd3f5
KeYang <kyang72@my.bcit.ca>
2022-07-18 15:42:15 -0700

    add style

--- javascripts/discourse/connectors/before-topic-list/solved-status-filter.js
+++ javascripts/discourse/connectors/before-topic-list/solved-status-filter.js
@@ -44,7 +44,7 @@ export default {
       const router = getOwner(this).lookup("router:main");
       if (newStatus ==="Unanswered") {
         console.log("unanswered");
-        router.transitionTo('max_posts', 1)
+        router.transitionTo('latest', 'max_post=1')
       } else {
         router.transitionTo({ queryParams: { solved: newStatus } });
       }
