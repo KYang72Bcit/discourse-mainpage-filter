d733f701c1d61bd11cef365cb47a29941b495e85
KeYang <kyang72@my.bcit.ca>
2022-07-18 17:43:03 -0700

    test router

--- javascripts/discourse/connectors/before-topic-list/solved-status-filter.js
+++ javascripts/discourse/connectors/before-topic-list/solved-status-filter.js
@@ -8,16 +8,16 @@ export default {
     if (
       !component.siteSettings.show_filter_by_solved_status ||
       router.currentPath === "discovery.categories"
-    ) {
-      return false;
-    } else if (component.siteSettings.allow_solved_on_all_topics) {
+    ) { return false;}
       return true;
-    } else {
-      const controller = getOwner(this).lookup(
-        "controller:navigation/category"
-      );
-      return controller && controller.get("category.enable_accepted_answers");
-    }
+    // } else if (component.siteSettings.allow_solved_on_all_topics) {
+    //   return true;
+    // } else {
+    //   const controller = getOwner(this).lookup(
+    //     "controller:navigation/category"
+    //   );
+    //   return controller && controller.get("category.enable_accepted_answers");
+    // }
   },
 
   setupComponent(args, component) {
@@ -44,10 +44,10 @@ export default {
   actions: {
     changeStatus(newStatus) {
       const router = getOwner(this).lookup("router:main");
-      if (newStatus ==="Unanswered") {
-        console.log("unanswered");
+      // if (newStatus ==="Unanswered") {
+      //   console.log("unanswered");
         
-      } 
+      // } 
         router.transitionTo({ queryParams: { solved: newStatus } });
       
       
